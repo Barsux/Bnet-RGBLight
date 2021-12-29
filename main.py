b0568fc3d024d08e4a3e5d5cfd0d1e2eaad1a20c
Basrux <yity.r7@gmail.com>
2021-12-29 13:57:48 +0300

    Class Color replaced by dict

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ client.clear()
 motherboard = client.get_devices_by_type(DeviceType.MOTHERBOARD)[0]
 mouse = client.get_devices_by_type(DeviceType.MOUSE)[0]
 
-def smooth(first_rgb, second_rgb, padding = 1):
+def set_color(rgb):
+    for i in range(3):
+        if rgb[i] < 0:
+            rgb[i] = 0
+    motherboard.set_color(RGBColor(*rgb))
+    sleep(0.000001)
+
+
+def smooth_set_color(first_rgb, second_rgb, padding = 1):
     rgb = list(first_rgb)
     red_high, red_low = second_rgb[0], first_rgb[0]
     green_high, green_low = second_rgb[1], first_rgb[1]
@@ -21,32 +29,13 @@ def smooth(first_rgb, second_rgb, padding = 1):
     for i in range(mn):
         for j in range(3):
             rgb[j] += abss[j]
-        setcolor(rgb)
-    setcolor(second_rgb)
-
-
-
+        set_color(rgb)
+    set_color(second_rgb)
 
 def hex2rgb(value):
     value = value.lstrip('#')
     lv = len(value)
     return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-
-def setcolor(rgb):
-    for i in range(3):
-        if rgb[i] < 0:
-            rgb[i] = 0
-    motherboard.set_color(RGBColor(*rgb))
-
-    sleep(0.000001)
-
-
-class Color():
-    def __init__(self, rgb, duration):
-        self.duration = duration
-        self.rgb = hex2rgb(rgb)
-
-colors = []
 
 def init():
     global colors
@@ -55,7 +44,10 @@ def init():
             steps = int(input("Enter number of profile steps : ")) + 1
             for i in range(1, steps):
                 hx, duration = input(f"Enter the hex of {i}'st\\rd step : "), int(input(f"Enter the duration of {i}'st\\rd step : "))
-                color = Color(hx, duration)
+                color = {
+                    "RGB": hex2rgb(hx),
+                    "Duration":duration
+                }
                 colors.append(color)
                 print(hx,color.duration, file=file)
     else:
@@ -63,18 +55,21 @@ def init():
             for line in file.readlines():
                 hx, duration = line.split(' ')
                 duration = float(duration)
-                color = Color(hx, duration)
+                color = {
+                    "RGB": hex2rgb(hx),
+                    "Duration": duration
+                }
                 colors.append(color)
 
 def mainloop():
     global colors
     while True:
         for i in range(1,len(colors)):
-            smooth(colors[i - 1].rgb, colors[i].rgb, 10)
-            sleep(colors[i].duration)
+            smooth_set_color(colors[i - 1]["RGB"], colors[i]["RGB"], 10)
+            sleep(colors[i]["Duration"])
         else:
-            smooth(colors[-1].rgb, colors[0].rgb, 10)
-            sleep(colors[0].duration)
+            smooth_set_color(colors[-1]["RGB"], colors[0]["RGB"], 10)
+            sleep(colors[0]["Duration"])
 
 
 
